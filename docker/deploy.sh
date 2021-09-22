0a7842320bec268917ffcf32e074cd2e8d961c36
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-09-22 13:35:24 +0200

    Use docker login action

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 label="${1:-latest}"
 tag="$DOCKERHUB_PROJECT:$1"
 
-echo "$DOCKERHUB_PASSWORD" | \
-  docker login -u "$DOCKERHUB_USER" --password-stdin
-
 # Build and tag image
 docker build --pull -t $tag -f docker/Dockerfile .
 docker push $tag
