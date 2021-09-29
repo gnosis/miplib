feeb26ab3d59029f8bce4239420831f5cc122cea
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-09-29 11:43:45 +0200

    Add docker image version to tag

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,9 +2,22 @@
 
 set -euo pipefail
 
-label="${1:-latest}"
-tag="$DOCKERHUB_PROJECT:$1"
+if [[ -z "${GITHUB_REF:-}" ]]; then
+    label=latest
+else
+    label=${GITHUB_REF#refs/*/}
+fi
+tag="$DOCKERHUB_PROJECT:$label"
 
 # Build and tag image
 docker build --pull -t $tag -f docker/Dockerfile .
 docker push $tag
+
+# additionally, tag the image with the version in `docker/version` file if the
+# deployment is not for a build triggered by a tag. This prevents issues with
+# breaking compatibility when updating the SCIP and Gurobi versions.
+if [[ "${GITHUB_REF:-}" != refs/tags/* ]]; then
+    full_tag="$DOCKERHUB_PROJECT:$(cat docker/version)-$label"
+    docker tag $tag $full_tag
+    docker push $full_tag
+fi
